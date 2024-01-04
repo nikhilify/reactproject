a5a88067407f9009887d9d6555ca8cab6d00c2de
nikhilify <144203855+nikhilify@users.noreply.github.com>
2024-01-04 23:38:24 +0530

    git commit
    commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import About from './components/About';
+import React,{useState} from 'react';
+// import TodoForm from './components/TodoForm';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+
+function App() {
+  const [mode, setMode ] = useState('light');
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+       setMode ('dark')
+       document.body.style.backgroundColor = 'black'
+    
+    }
+    
+    else{
+      setMode ('light')
+      document.body.style.backgroundColor = 'white'
+
+    }
+  }
+  return (
+<>
+  <Router>
+  <Navbar title="textutils" aboutText="About Us" mode={mode} toggleMode={toggleMode}/>
+  <div className='container my-3' >
+ 
+    {/* <About mode={mode}></About> */}
+        <Routes>
+          <Route exact path="/about" element={<About mode={mode}/>}>
+          </Route>
+          <Route exact path="/" element={<TextForm heading="Enter text to analyze" upper="Convert to Uppercase" mode={mode}/>}>
+
+          </Route>
+        </Routes>
+    </div>
+    </Router>
+</>
+  )
+};
+
+export default App;
